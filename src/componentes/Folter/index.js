68c22ed639e77d85e728733e3cc8b0f65ac8d57b
Lokerodrigues <wadsondragneel@gmail.com>
2023-03-06 22:27:53 -0300

    commit first

---
+++ src/componentes/Folter/index.js
@@ -0,0 +1,32 @@
+import './index.css'
+
+function Folter() {
+    return(
+        <footer className="folter">
+
+        <img src="/imagens-rodape/folter.png" alt="O rodapé do site" />
+         
+
+        <div class= "redes">
+            <img src="/imagens-rodape/fb.png" alt="facebook" />
+            <img src="/imagens-rodape/ig.png" alt="instragram" />
+            <img src="/imagens-rodape/tw.png" alt="twitter" />      
+        </div>
+
+        <div>
+            <h5> Desenvolvido por Wadson Rodrigues</h5> 
+        </div>
+
+       
+
+        </footer>
+    )
+
+    
+}
+
+
+
+
+
+export default Folter
